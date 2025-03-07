40d22a74cd124eb4eb22b057cf36196b33d3a9c2
samm1616 <sam1616@mit.edu>
2025-03-07 17:56:39 -0500

    Add lab3 correct code

---
+++ lab3/wall_follower/setup.py
@@ -0,0 +1,30 @@
+import glob
+import os
+from setuptools import find_packages, setup
+
+package_name = 'wall_follower'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/wall_follower/launch', glob.glob(os.path.join('launch', '*launch.py'))),
+        ('share/' + package_name + '/config', ['config/wall_follower_params.yaml']),
+        ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='root@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "wall_follower = wall_follower.wall_follower:main",
+            ],
+    },
+)
