cb7a0419086d5d9a9994089bb082657b6d7a235a
Sammy Krem <skrem@mit.edu>
2025-03-10 00:36:30 -0400

    update

--- lab3/safety_controller/safety_controller/safety_controller.py
+++ lab3/safety_controller/safety_controller/safety_controller.py
@@ -50,7 +50,7 @@ class SafetyController(Node):
 
 
         else:
-            self.estop_dist = 0.55 * drive_msg.drive.speed
+            self.estop_dist = 0.57 * drive_msg.drive.speed
             if self.i == 100:
                 self.get_logger().info(f"speed is {drive_msg.drive.speed}")
       
