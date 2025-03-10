4668c51fea7eb7c5fb7b77ae0188765906461992
Sammy Krem <skrem@mit.edu>
2025-03-10 00:27:58 -0400

    update

--- lab3/safety_controller/safety_controller/safety_controller.py
+++ lab3/safety_controller/safety_controller/safety_controller.py
@@ -19,9 +19,9 @@ class SafetyController(Node):
         self.DRIVE_TOPIC = self.get_parameter('drive_topic').get_parameter_value().string_value
 
         self.lidar_dist = 0.1  # Distance from lidar to front of car
-        self.ang_bounds = -np.pi/7, np.pi/7
+        self.ang_bounds = -np.pi/6, np.pi/6
         # self.car_width = 0.31
-        self.count_threshold = 30  # Define threshold for stopping
+        self.count_threshold = 20  # Define threshold for stopping
 
         # ROS 2 Subscribers & Publishers
         self.drive_sub = self.create_subscription(
